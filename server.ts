63c4322d82da6447b76ebf8bf6bd7db629ac5323
John Ogbonna <johnny.ogb@gmail.com>
2024-02-09 00:18:52 -0700

    Initialized git, server app for chat socket

---
+++ server.ts
@@ -0,0 +1,26 @@
+const expressWs = require('express-ws')
+const express = require('express')
+const WebSocketChatRoute = require('./dist/routes/chat')
+const CheckUserRoute = require('./dist/routes/checkUser')
+const bodyParser = require('body-parser');
+const PORT: number = 3000
+import webSocketServer from './dist/routes/chat'
+import WebSocket from 'ws'
+import http from 'http';
+const cors = require('cors');
+
+
+const app = express();
+app.use(cors())
+app.use(bodyParser.json())
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+
+webSocketServer(wss)
+
+app.use('/check', CheckUserRoute)
+
+server.listen(PORT, ()=>{
+    console.log('server runnning')
+})
